37853325d937c713023bfc2a12b667c1a8fe0347
nazmus767921 <nazmus767921@gmail.com>
2024-05-31 15:22:25 +0600

    feat(app): Mobile Responsiveness

--- src/pages/MenuPage/index.tsx
+++ src/pages/MenuPage/index.tsx
@@ -2,6 +2,7 @@ import Banner from "@components/Banner/Banner";
 import FoodCard from "@components/FoodCard/FoodCard";
 import Footer from "@components/Footer/Footer";
 import Nav from "@components/Nav/Nav";
+import SelectField, { Option } from "@components/form/SelectField/SelectField";
 import { nanoid } from "@reduxjs/toolkit";
 import { utilizeModuleClasses } from "easy-css-modules";
 import { memo, useMemo, useState } from "react";
@@ -10,11 +11,38 @@ import styles from "./styles/Menupage.module.scss";
 const $_ = utilizeModuleClasses(styles);
 
 const MenuPage = () => {
+  const options: Option[] = [
+    {
+      id: nanoid(),
+      value: "Show All",
+    },
+    {
+      id: nanoid(),
+      value: "Refreshing Breakfast",
+    },
+    {
+      id: nanoid(),
+      value: "Lunch Box",
+    },
+    {
+      id: nanoid(),
+      value: "Dinner Delight",
+    },
+  ];
   return (
     <>
       <Nav slot={<Banner />} />
       <section className={$_("foodMenuSection @container")}>
         <Sidebar />
+        <div className={$_("foodMenuSection__filters")}>
+          <SelectField
+            color="var(--brand)"
+            id={nanoid()}
+            name="menu"
+            label="Menu"
+            options={options}
+          />
+        </div>
         <FoodMenu />
       </section>
       <Footer />
@@ -90,19 +118,10 @@ const FoodMenu = () => {
   const ViewSorter = memo(() => {
     return (
       <div className={$_("foodMenu__viewSorter")}>
-        <div>
-          <h3 className={$_("foodMenu__categoryTitle @isBrand")}>Breakfast</h3>
-          {/* <p
-            className={$_("@is--small")}
-            opacity={0.6}
-            color={"var(--accent)"}
-          >
-            Showing Non-vegetarian
-          </p> */}
-        </div>
-        <p className={$_("foodMenu__pageStatusText @is--small")}>
-          Page 1 of 15
-        </p>
+        <h3 className={$_("foodMenu__categoryTitle @isBrand")}>
+          Refreshing Breakfast
+        </h3>
+
         <p className={$_("foodMenu__pageStatusText @is--small")}>
           Page 1 of 15
         </p>
@@ -114,42 +133,42 @@ const FoodMenu = () => {
     return (
       <div className={$_("foodMenu__foodCards")}>
         <FoodCard
+          variant={2}
           name="Sahi Kebab"
           description="Best meat you can get between your teeth."
           image="sahi_kebab2.png"
           price={2.77}
         />
         <FoodCard
+          variant={2}
           name="Sahi Kebab"
           description="Best meat you can get between your teeth."
           image="sahi_kebab2.png"
           price={2.77}
         />
         <FoodCard
+          variant={2}
           name="Sahi Kebab"
           description="Best meat you can get between your teeth."
           image="sahi_kebab2.png"
           price={2.77}
         />
         <FoodCard
+          variant={2}
           name="Sahi Kebab"
           description="Best meat you can get between your teeth."
           image="sahi_kebab2.png"
           price={2.77}
         />
         <FoodCard
+          variant={2}
           name="Sahi Kebab"
           description="Best meat you can get between your teeth."
           image="sahi_kebab2.png"
           price={2.77}
         />
         <FoodCard
-          name="Sahi Kebab"
-          description="Best meat you can get between your teeth."
-          image="sahi_kebab2.png"
-          price={2.77}
-        />
-        <FoodCard
+          variant={2}
           name="Sahi Kebab"
           description="Best meat you can get between your teeth."
           image="sahi_kebab2.png"
